2028a1fbbca210cb05128e632a6f9144896d8c86
Maja Sigfeldt <msigfeldt@gmail.com>
2026-01-10 21:10:51 +0100

    Add project files.

---
+++ BudgetHelperClassLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetHelperClassLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ aBudgetHelperTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace aBudgetHelperTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
