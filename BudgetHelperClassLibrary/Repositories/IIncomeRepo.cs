4068d696ce6bc40bb5ebac838cf95cdb1e092931
Maja Sigfeldt <msigfeldt@gmail.com>
2026-01-12 16:02:47 +0100

    saftey commit

---
+++ BudgetHelperClassLibrary/Repositories/IIncomeRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetHelperClassLibrary.Repositories
+{
+    public interface IIncomeRepo
+    {
+        //Fixa så alla crudmetoder finns
+    }
+}
