bf7312121bd75ccef62a3aa90202c644246ad3ad
Maja Sigfeldt <msigfeldt@gmail.com>
2026-01-13 22:55:35 +0100

    Implemented ICommand

---
+++ BudgetHelperMS/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BudgetHelperMS.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _executeCommand;
+        private readonly Func<bool>? _canExecuteCheck;
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action executeCommand, Func<bool>? canExecuteCheck=null)
+        {
+            _executeCommand = executeCommand;
+            _canExecuteCheck = canExecuteCheck;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecuteCheck?.Invoke() ?? true;
+
+        public void Execute(object? parameter)=>_executeCommand();
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
